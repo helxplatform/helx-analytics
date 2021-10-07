330b3f4a74cba598b63b780b316229d23e4044f6
Griffin Roupe <groupe@dhcp-152-23-40-126.wireless-1x.unc.edu>
2021-10-07 18:47:41 -0400

    Fixes error in exports

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,4 @@
-export { default as GAAnalytics, GASetupData } from './GAAnalytics';
-export { default as MixPanelAnalytics, MixPanelSetupData } from './MixPanelAnalytics';
+export { default as GAAnalytics } from './GAAnalytics';
+export { default as MixPanelAnalytics } from './MixPanelAnalytics';
 export { default as NoAnalytics } from './NoAnalytics';
 //# sourceMappingURL=index.d.ts.map
